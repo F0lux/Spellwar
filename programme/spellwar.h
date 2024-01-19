0a1ed702e178723d681cd619faef6e10c8d741b6
Masson Rafael <152910353+F0lux@users.noreply.github.com>
2024-01-19 04:40:53 +0100

    coments 95% done

--- programme/spellwar.h
+++ programme/spellwar.h
@@ -54,23 +54,23 @@ struct Spellwar
 // Procedure permettant de creer la zone de jeu dans une grille de jeu de spellwar
 void creerZoneJeu(Spellwar &grille);
 
-/* Fonction qui retourne un enregistrement de joueur, prend en parametre deplCeTour qui si vrai, 
+/* Fonction qui retourne un enregistrement de joueur, prend en parametre deplCeTour qui si vrai,
 la valeur de deplaceCeTour de joueur sera vrai ; ce parametre vaut faux par defaut */
 ElementDeJeu creerJoueur(bool deplCeTour = false);
 
-/* Fonction qui retourne un enregistrement d'arcaflamme, prend en parametre deplCeTour qui si vrai, 
+/* Fonction qui retourne un enregistrement d'arcaflamme, prend en parametre deplCeTour qui si vrai,
 la valeur de deplaceCeTour de l'arcaflamme sera vrai ; ce parametre vaut faux par defaut */
 ElementDeJeu creerArcaflamme(bool deplCeTour = false);
 
-/* Fonction qui retourne un enregistrement de necrogriffe, prend en parametre deplCeTour qui si vrai, 
+/* Fonction qui retourne un enregistrement de necrogriffe, prend en parametre deplCeTour qui si vrai,
 la valeur de deplaceCeTour de necrogriffe sera vrai ; ce parametre vaut faux par defaut */
 ElementDeJeu creerNecrogriffe(bool deplCeTour = false);
 
-/* Fonction qui retourne un enregistrement d'eclair, prend en parametre deplCeTour qui si vrai, 
+/* Fonction qui retourne un enregistrement d'eclair, prend en parametre deplCeTour qui si vrai,
 la valeur de deplaceCeTour de l'eclair sera vrai ; ce parametre vaut faux par defaut */
 ElementDeJeu creerEclair(bool deplCeTour = false);
 
-/* Fonction qui retourne un enregistrement de flamme, prend en parametre deplCeTour qui si vrai, 
+/* Fonction qui retourne un enregistrement de flamme, prend en parametre deplCeTour qui si vrai,
 la valeur de deplaceCeTour de flamme sera vrai ; ce parametre vaut faux par defaut */
 ElementDeJeu creerFlamme(bool deplCeTour = false);
 
@@ -92,109 +92,80 @@ SituationFinJeu obtenirFinJeu(const Spellwar &grille);
 // Procedure qui definit la fin de la partie de grille a finJ
 void definirFinJeu(Spellwar &grille, const SituationFinJeu &finJ);
 
-// Procedure qui definit l'element en iLigne iColonne de la zone de jeu grille a element 
+// Procedure qui definit l'element en iLigne iColonne de la zone de jeu grille a element
 void definirElement(Spellwar &grille, unsigned int iLigne, unsigned int iColonne, ElementDeJeu element);
 
-/* Procedure qui permet de jouer un tour de spellwar dans la zone de jeu grille 
+/* Procedure qui permet de jouer un tour de spellwar dans la zone de jeu grille
 et qui prend en parametre l'action courante du joueur actionJ et le nombre d'aversaires encore en vie adversairesViv */
 void jouerUnTour(Spellwar &grille, char actionJ, unsigned int &adversairesViv);
 
-/* Procedure qui permet de positionner le joueur et les adversaires du jeu spellwar 
+/* Procedure qui permet de positionner le joueur et les adversaires du jeu spellwar
 Prend en paramtre grille la grille de jeu a modifier */
 void positionnerPersonnages(Spellwar &grille);
 
 // Procedure qui permet d'afficher la zone de jeu grille et les actions possibles pour le joueur
 void afficherPlateauEtInformations(const Spellwar &grille);
 
-/**
- * @brief Permet de supprimer les marques de collision du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- */
+// Procedure qui permet de supprimer les marques de collision dans la zone de jeu grille
 void supprimerMarqueCollision(Spellwar &grille);
 
-/**
- * @brief Permet d'executer l'action saisie pour le joueur du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- * @param [in] actionJ l'action du joueur
- * @param [in] iLigne l'indice de la ligne de l'element
- * @param [in] iColonne l'indice de la colonne de l'element
- * @param [in] adversairesViv nombre d'aversaires encore en vie
- */
+/* Procedure qui permet d'executer l'action saisie pour le joueur du jeu spellwar
+ * Prend en parametre la grille de jeu a modifier, actionJ l'action du joueur, iLigne l'indice de la ligne de l'element,
+ * iColonne l'indice de la colonne de l'element et adversairesViv le nombre d'adversaires encore en vie */
 void jouerJoueur(Spellwar &grille, char actionJ, unsigned int iLigne, unsigned int iColonne, unsigned int &adversairesViv);
 
-/**
- * @brief Permet de faire jouer un arcaflamme du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- * @param [in] iLigne l'indice de la ligne de l'element
- * @param [in] iColonne l'indice de la colonne de l'element
- * @param [in] adversairesViv nombre d'adversaires encore en vie
- */
+/* Procedure qui permet de faire jouer un arcaflamme du jeu spellwar
+ * Prend en parametre la grille de jeu a modifier, iLigne l'indice de la ligne de l'element,
+ * iColonne l'indice de la colonne de l'element et adversairesViv le nombre d'adversaires encore en vie */
 void jouerArcaflamme(Spellwar &grille, unsigned int iLigne, unsigned int iColonne, unsigned int &adversairesViv);
 
-/**
- * @brief Permet de faire jouer un necrogriffe du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- * @param [in] actionJ l'action du joueur
- * @param [in] iLigne l'indice de la ligne de l'element
- * @param [in] iColonne l'indice de la colonne de l'element
- * @param [in] adversairesViv nombre d'adversaires encore en vie
- */
+/* Procedure qui permet de faire jouer un necrogriffe du jeu spellwar
+ * Prend en parametre la grille de jeu a modifier, actionJ l'action du joueur, iLigne l'indice de la ligne de l'element,
+ * iColonne l'indice de la colonne de l'element et adversairesViv le nombre d'adversaires encore en vie */
 void jouerNecrogriffe(Spellwar &grille, char actionJ, unsigned int iLigne, unsigned int iColonne, unsigned int &adversairesViv);
 
-/**
- * @brief Permet de deplacer un eclair du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- * @param [in] iLigne l'indice de la ligne de l'element
- * @param [in] iColonne l'indice de la colonne de l'element
- * @param [in] adversairesViv nombre d'adversaires encore en vie
- */
+/* Procedure qui permet de deplacer un eclair du jeu spellwar
+ * Prend en parametre la grille de jeu a modifier, iLigne l'indice de la ligne de l'element,
+ * iColonne l'indice de la colonne de l'element et adversairesViv le nombre d'adversaires encore en vie */
 void deplacerEclair(Spellwar &grille, unsigned int iLigne, unsigned int iColonne, unsigned int &adversairesViv);
 
-/**
- * @brief Permet de deplacer une flamme du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- * @param [in] iLigne l'indice de la ligne de l'element
- * @param [in] iColonne l'indice de la colonne de l'element
- * @param [in] adversairesViv nombre d'adversaires encore en vie
- */
+/* Procedure qui permet de deplacer une flamme du jeu spellwar
+ * Prend en parametre la grille de jeu a modifier, iLigne l'indice de la ligne de l'element,
+ * iColonne l'indice de la colonne de l'element et adversairesViv le nombre d'adversaires encore en vie */
 void deplacerFlamme(Spellwar &grille, unsigned int iLigne, unsigned int iColonne, unsigned int &adversairesViv);
 
-/**
- * @brief Permet de mettre a faux deplaceCeTour pour tout les elements du jeu spellwar
- *
- * @param [in] grille la grille de jeu a modifier
- */
+// Permet de mettre a faux deplaceCeTour pour tout les elements du jeu spellwar dans la zone de jeu grille
 void reinitialiserDeplacements(Spellwar &grille);
 
+// 
 bool estJoueurPasDeplCeTour(Spellwar &grille, unsigned int iLigne, unsigned int iColonne);
 
+//
 bool estArcaflammePasDeplCeTour(Spellwar &grille, unsigned int iLigne, unsigned int iColonne);
 
+//
 bool estNecrogriffePasDeplCeTour(Spellwar &grille, unsigned int iLigne, unsigned int iColonne);
 
+//
 bool estEclairPasDeplCeTour(Spellwar &grille, unsigned int iLigne, unsigned int iColonne);
 
+//
 bool estFlammePasDeplCeTour(Spellwar &grille, unsigned int iLigne, unsigned int iColonne);
 
+//
 bool estSurPremiereColonne(unsigned int iColonne);
 
+//
 bool estSurDerniereColonne(unsigned int iColonne);
 
+//
 bool estSurPremiereOuDerniereLigne(unsigned int iLigne);
 
+//
 bool estSurPremiereColonnePasPremiereLignePasDerniereLigne(unsigned int iLigne, unsigned int iColonne);
 
-/**
- * @brief Permet d'afficher un message expliquant la raison de la fin de la partie du jeu spellwar
- *
- * @param [in] grille la grille de jeu qui contient la raison de la fin de la partie
- */
+/* Permet d'afficher un message expliquant la raison de la fin de la partie du jeu spellwar
+ * Prend en parametre la zone de jeu grille qui contient la raison de la fin de la partie */
 void finirLaPartie(const Spellwar &grille);
 
 #endif // SPELLWAR_H
